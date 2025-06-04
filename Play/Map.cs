5ed566c41fa9036e38f2dfd3978726e3eee1494e
judygreenplato <judygreenplato@gmail.com>
2025-06-04 17:29:23 +0200

    Added cell

--- Play/Map.cs
+++ Play/Map.cs
@@ -1,11 +1,14 @@
 ﻿internal class Map
 {
+    private Cell[,] cells;
     private int height;
     private int width;
 
-    public Map(int height, int width)
+    public Map(int width, int height)
     {
         this.height = height;
         this.width = width;
+
+        cells=new Cell[width,height];
     }
 }
