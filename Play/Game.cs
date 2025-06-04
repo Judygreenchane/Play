8ee541caee7c7c083ce8ee09838d1b90f1efd091
judygreenplato <judygreenplato@gmail.com>
2025-06-04 17:09:36 +0200

    started on Game

---
+++ Play/Game.cs
@@ -0,0 +1,29 @@
+﻿
+
+using System.Data;
+
+internal class Game
+{
+    private Map map = null!;
+    private Hero hero = null!;
+
+    internal void Run()
+    {
+        Initialize();
+        Play();
+    }
+
+    private void Play()
+    {
+        // ToDo : Read from  cff Config may be
+        throw new NotImplementedException();
+    }
+
+    private void Initialize()
+    {
+        // ToDo : Read from Config may be
+         map=new Map(width: 10,height:10);
+         hero = new Hero();
+
+    }
+}
