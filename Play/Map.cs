71374d3ccd81f20424b366a9a90ce613e19d8c15
judygreenplato <judygreenplato@gmail.com>
2025-06-05 16:30:02 +0200

    Init map

--- Play/Map.cs
+++ Play/Map.cs
@@ -9,6 +9,14 @@
         this.height = height;
         this.width = width;
 
-        cells=new Cell[width,height];
+        cells = new Cell[width, height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                cells[y, x] = new Cell();
+            }
+        }
     }
 }
